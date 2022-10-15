168182a5754c5a7c1f8516e4696306b6a8d9f6fd
Dev-Devarsh <devarshpanchal2001@gmail.com>
2022-10-15 16:50:54 +0530

    delete opration

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // start code read from mongodb.js
 
 const dbConnection = require('./mongodb');  
-const listname = ['dev', 'test', 'test 2' ,'khgsfbgiuy']
-async function updateDatabase() {
-    let data = await dbConnection();
-// update feild one by one using for loop 
-    for (let i = 0; i < 4; i++) {
-        let result = await data.updateOne(
-            {name: `devarsh ${i}`},{
-                $set : {name: `devarsh ${i}` , age : 685981}
-            }
-        );//update only one feild in database
-     console.warn(result);
 
-    }
-    let result1 = await data.updateMany(
-        {name: 'devarsh primus'},{
-            $set : {name: 'devarsh primus' , age : 685981}
+const listname = ['dev', 'test']
+async function deleteDatabase() {
+    let data = await dbConnection();
+    // delete feild one by one using for loop
+
+    for (let i = 0; i < listname.length; i++) {
+
+        let result = await data.deleteOne({name: `devarsh ${listname[i]}`});//delete only one feild in database
+// if deleted count is greater than zero and acknowledged is true it means data delted scuccess fully from database
+/* if data isn't exist in database for that acknowledged will be true but 
+   deleted count will be zero */
+        if(result.deletedCount != 0 && result.acknowledged){
+            console.warn(result);    
         }
-    );//update all feilds in databases
+    }
+    //delete all feilds in databases
+    let result1 = await data.deleteMany({name: 'devarsh primus'});
 }
 
-updateDatabase();
+deleteDatabase()
