310702b583eb3999239c43f6b2672c8d6d015744
Dev-Devarsh <devarshpanchal2001@gmail.com>
2022-10-15 12:49:39 +0530

    update opration

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 // start code read from mongodb.js
 
-const dbConnection = require('./mongodb');
-
-//toArray will return promise(Future) objects 
-//.then method is used to handle promise(Future) objects
-// (method 1) how to handle promise(Future) objects
-dbConnection().then((result) => {
-    result.find().toArray().then((data)=> {
-            //console.warn(data);
-        })
-    });
-
-
-// how to make function without using function keyWord
-// (method 2) how to handle promise(Future) objects
- const main = async() =>{
+const dbConnection = require('./mongodb');  
+const listname = ['dev', 'test', 'test 2' ,'khgsfbgiuy']
+async function updateDatabase() {
     let data = await dbConnection();
-    data = await data.find().toArray();
-    console.warn(data);
+// update feild one by one using for loop 
+    for (let i = 0; i < 4; i++) {
+        let result = await data.updateOne(
+            {name: `devarsh ${i}`},{
+                $set : {name: `devarsh ${i}` , age : 685981}
+            }
+        );//update only one feild in database
+     console.warn(result);
+
+    }
+    let result1 = await data.updateMany(
+        {name: 'devarsh primus'},{
+            $set : {name: 'devarsh primus' , age : 685981}
+        }
+    );//update all feilds in databases
 }
 
-main();
+updateDatabase();
