0bcf8d76c624c202cb36367619821bf0d08be74d
Dev-Devarsh <devarshpanchal2001@gmail.com>
2022-10-16 22:12:46 +0530

    init

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const http = require('http');
 const express = require('express');
 const dbConnect = require('./mongodb');
+const mongodb = require('mongodb');
 const app =  express();
 
 app.use(express.json()); // to convert post api data into json
 
-app.get('/', async function (req, res){ // or app.get('/', async (req, res)=>{}
+app.delete('/',async function (req, res) {
     let data = await dbConnect();
-    data = await data.find().toArray();
-    // console.log(data)
-    res.send(data);   
-    
-});
-app.post('/', async function (req, res){ // or app.get('/', async (req, res)=>{}
-    let data = await dbConnect();
-    let result = await data.insertOne(req.body)
-    console.log(req.body);
-    res.send(result)
-});
+    // only use this method
+    let result  = await data.deleteOne({_id: new mongodb.ObjectId(req.params.id)});
+    console.log(result);
+
+    if (result.acknowledged) {
+        res.send({"status": 200});
+        
+    } else {
+        res.send({"status": 404});
+        
+    }
+}); 
  
 app.listen(4500);
