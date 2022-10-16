97f2360a7785fcf4746e06a3dab66f6c5d7c1174
Dev-Devarsh <devarshpanchal2001@gmail.com>
2022-10-16 10:56:12 +0530

    .

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const express = require('express');
 const dbConnect = require('./mongodb');
 const app =  express();
 
+app.use(express.json()); // to convert post api data into json
+
 app.get('/', async function (req, res){ // or app.get('/', async (req, res)=>{}
     let data = await dbConnect();
     data = await data.find().toArray();
-    console.log(data)
-    res.send(data); 
+    // console.log(data)
+    res.send(data);   
+    
+});
+app.post('/', async function (req, res){ // or app.get('/', async (req, res)=>{}
+ 
+    console.log(req.body);
+    res.send({status : '200'})
     
 });
  
