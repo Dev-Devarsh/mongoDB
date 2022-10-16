fd253a38fd81c3039ac470d29ebe97e85a763783
Dev-Devarsh <devarshpanchal2001@gmail.com>
2022-10-16 22:25:23 +0530

    conclision

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // start code read from mongodb.js
-
+/* cons : 
+    1)  mongoDb package there is no schema , means we can not stop extra field added by user side
+        it means if in a collection there is 4 field so we can't control 5th field creation i.e it can be create by user side
+     
+    2) we can't control fields type , it means if user enter age as String so age type will be chnaged in db for that user */
 const dbConnection = require('./mongodb');
 
 //toArray will return promise(Future) objects 
