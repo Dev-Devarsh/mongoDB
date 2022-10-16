9ac905d06edd9f1fa80aa2fec2db5f150b9502b7
Dev-Devarsh <devarshpanchal2001@gmail.com>
2022-10-16 10:19:58 +0530

    end

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-// start code read from mongodb.js
+const http = require('http');
+const express = require('express');
+const dbConnect = require('./mongodb');
+const app =  express();
 
-const dbConnection = require('./mongodb');  
-
-const listname = ['dev', 'test']
-async function deleteDatabase() {
-    let data = await dbConnection();
-    // delete feild one by one using for loop
-
-    for (let i = 0; i < listname.length; i++) {
-
-        let result = await data.deleteOne({name: `devarsh ${listname[i]}`});//delete only one feild in database
-// if deleted count is greater than zero and acknowledged is true it means data delted scuccess fully from database
-/* if data isn't exist in database for that acknowledged will be true but 
-   deleted count will be zero */
-        if(result.deletedCount != 0 && result.acknowledged){
-            console.warn(result);    
-        }
-    }
-    //delete all feilds in databases
-    let result1 = await data.deleteMany({name: 'devarsh primus'});
-}
-
-deleteDatabase()
+app.get('/', async function (req, res){ // or app.get('/', async (req, res)=>{}
+    let data = await dbConnect();
+    data = await data.find().toArray();
+    console.log(data)
+    res.send(data); 
+    
+});
+ 
+app.listen(4500);
