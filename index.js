f9d2f8a7c2db22c6406039ee915ca914e47e2604
Dev-Devarsh <devarshpanchal2001@gmail.com>
2022-10-17 18:40:42 +0530

    .

--- index.js
+++ index.js
@@ -3,20 +3,31 @@
 // (1) schemas are used define fiels in db
 // (2) model by using schemas connects node-JS to mongoDB
 
-
-const express = require('express');
-require('./config');
-const profileSchema = require('./profile');
-
+const express = require("express");
+require("./config");
+const profileSchema = require("./profile");
 
 const app = express();
 app.use(express.json()); // to convert post api data into json
 
-app.get('/list', async (req, resp) => {
-    let data = await profileSchema.find();
+app.get("/list", async (req, resp) => {
+  let data = await profileSchema.find();
+  console.log(data);
+  resp.send(data);
+});
+
+app.put("/update", async (req, resp) => {
+  let data = await profileSchema.updateOne(
+    { name: "devarsh 1232" },
+    { $set: { name: "dev 132" } }
+  );
+  console.log(data);
+  resp.send(data);
+});
+
+app.delete("/delete", async (req, resp) => {
+    let data = await profileSchema.deleteOne({name : 'devarsh 525'})
     console.log(data);
     resp.send(data);
 });
-
-app
-app.listen(5000)
+app.listen(5000);
