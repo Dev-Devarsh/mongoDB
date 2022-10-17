dc807be9ac0e91ab76376193b6ef2b0d3d2fd975
Dev-Devarsh <devarshpanchal2001@gmail.com>
2022-10-17 18:21:01 +0530

    get app

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 // (2) model by using schemas connects node-JS to mongoDB
 
 
-const { response } = require('express');
 const express = require('express');
 require('./config');
 const profileSchema = require('./profile');
@@ -13,11 +12,10 @@ const profileSchema = require('./profile');
 const app = express();
 app.use(express.json()); // to convert post api data into json
 
-app.post('/create', async function (req, resp) {
-    let data = new profileSchema(req.body);
-    let result = await data.save();
-    console.log(req.body);
-    resp.send(result);
+app.get('/', async (req, resp) => {
+    let data = await profileSchema.find();
+    console.log(data);
+    resp.send(data);
 });
 
 app.listen(5000)
